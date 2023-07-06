6970494a3dc5a70d08ee733f8ec7bb1534ceafb4
ZakariyeAden <zakariye5417@gmail.com>
2023-07-06 12:20:20 -0500

    starting files

---
+++ src/components/ShopForm/ShopForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ShopForm = () => {
+  return (
+    <div>ShopForm</div>
+  )
+}
+
+export default ShopForm
---
+++ src/components/ShopItem/ShopItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ShopItem = () => {
+  return (
+    <div>ShopItem</div>
+  )
+}
+
+export default ShopItem
