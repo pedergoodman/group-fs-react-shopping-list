3a3c12347bf1ce011161ac2fb4cf51c21bed4663
Peder Goodman <peder.goodman@gmail.com>
2023-07-06 14:35:29 -0500

    change DB name, import axios to App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-
+import axios from 'axios'
 import Header from '../Header/Header.jsx'
 import Title from '../Title/Title.jsx';
 import './App.css';
