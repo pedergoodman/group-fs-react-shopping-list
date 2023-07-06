b4483632a9cd73b0c6a97af862ab5e2647119df3
ZakariyeAden <zakariye5417@gmail.com>
2023-07-06 12:09:17 -0500

    DB table

--- database.sql
+++ database.sql
@@ -1,2 +1,10 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+
+CREATE TABLE "shop" (
+	"id" SERIAL PRIMARY KEY,
+	"item" varchar(80) NOT NULL,
+	"quantity" FLOAT NOT NULL,
+	"unit" varchar(20) NOT NULL,
+	"isPurchased" BOOLEAN DEFAULT FALSE
+);
