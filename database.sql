2e17cbebf0a4c1866b82d1c6ce0ada23de541503
Peder Goodman <peder.goodman@gmail.com>
2023-07-06 12:10:56 -0500

    add sample data

--- database.sql
+++ database.sql
@@ -8,3 +8,15 @@ CREATE TABLE "shop" (
 	"unit" varchar(20) NOT NULL,
 	"isPurchased" BOOLEAN DEFAULT FALSE
 );
+
+
+INSERT INTO "shop" (item, quantity, unit)
+VALUES
+  ('Milk', '2', 'liters'),
+  ('Bread', '1', 'loaf'),
+  ('Eggs', '1', 'dozen'),
+  ('Chicken', '500', 'grams'),
+  ('Rice', '2', 'kilograms'),
+  ('Tomatoes', '5', 'pieces'),
+  ('Apples', '6', 'pieces'),
+  ('Potatoes', '3', 'kilograms');
