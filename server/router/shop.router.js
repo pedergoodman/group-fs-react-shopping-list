5332288199584ee7153a4bb3af8988839aee3f4b
Peder Goodman <peder.goodman@gmail.com>
2023-07-06 14:06:58 -0500

    add GET request in App, GET route in router

--- server/router/shop.router.js
+++ server/router/shop.router.js
@@ -5,6 +5,26 @@ const pool = require('../modules/pool')
 
 
 // GET route -- load list, sort by purchased, then by Alphabetical
+router.get('/', (req, res) => {
+
+  // select all items, ordered by isPurchased first (false should come first)
+  // then by item, We can change ASC or DES later if needed. 
+  const sqlText = `SELECT * FROM "shop" ORDER BY "isPurchased", "item"`
+
+  pool.query(sqlText)
+    .then((result) => {
+      console.log('DB items received!');
+
+      // rows grabbed with results.rows
+      res.send(result.rows)
+    }).catch((err) => {
+      console.log(`Error making DB query, ${sqlText}`, err );
+      res.sendStatus(500)
+    });
+
+}); // end GET route
+
+
 
 
 // POST route
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Header from '../Header/Header.jsx'
 import './App.css';
 
 
+
+
+
+
+
+
 function App() {
+
+    const [ shoppingList, setShoppingList ] = useState([]);
+
+    const getShoppingList = () => {
+        axios.get('/shopping-list')
+        .then((response) => {
+            console.log('response is:', response);
+    
+            console.log('response data is:', response.data);
+        }).catch((err) => {
+            console.log('Error GETing shopping list:', err);
+            alert('Error GETing shopping list')
+        });
+    }; // end getShoppingList
+
+
+    useEffect( () => {
+        getShoppingList();
+    }, []);
+
+
     return (
         <div className="App">
             <Header />
